7f3b7f576cf46b1fcf9ba310119f003bf558c156
Luiz Felipe Gomes Magalhães <lowwer44@hotmail.com>
2025-09-01 17:15:45 +0000

    1 Trabalho Parcial

---
+++ projeto-integrador/src/main/java/entidades/Itemvenda.java
@@ -0,0 +1,21 @@
+package entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Itemvenda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer codigo;
+
+    public Double valorParcial;
+
+    public Integer quantidadeParcial;
+
+    @ManyToOne
+    public Produto produto;
+}
